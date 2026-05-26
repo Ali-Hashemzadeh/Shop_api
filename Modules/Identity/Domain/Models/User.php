ac68df080dd97b85354a9553fce089b374644e53
Ali MELMEDAS <aliimail623@gmail.com>
2026-05-27 01:08:40 +0330

    Adding Role Trait To User Model

--- Modules/Identity/Domain/Models/User.php
+++ Modules/Identity/Domain/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+
+    protected string $guard_name = 'web';
     protected $fillable = [
         'name',
         'email',
